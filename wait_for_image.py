852664e9b2b7fe80a19a920393607b050bb303e5
WonYeongjun <1014wonyoung@snu.ac.kr>
2025-02-12 14:48:58 +0900

    주석 없애고, gif는 감지 안 하게 함

--- wait_for_image.py
+++ wait_for_image.py
@@ -4,11 +4,11 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import threading
 
-# 전역 변수 설정
-last_executed = {}  # 파일별 마지막 실행 시간 저장
-EXECUTION_DELAY = 2  # 중복 실행 방지 대기 시간 (초)
-is_running = False  # 파이썬 코드 실행 중 여부
-lock = threading.Lock()  # 스레드 안전성을 위한 락
+
+last_executed = {}
+EXECUTION_DELAY = 2
+is_running = False
+lock = threading.Lock()
 
 
 class ImageFileEventHandler(FileSystemEventHandler):
@@ -18,22 +18,19 @@ class ImageFileEventHandler(FileSystemEventHandler):
             if is_running:
                 return
 
-            if event.src_path.lower().endswith((".jpg", ".jpeg", ".png", ".gif")):
-                time.sleep(0.5)  # 파일 저장이 완료되도록 대기
+            if event.src_path.lower().endswith((".jpg", ".jpeg", ".png")):
+                time.sleep(0.5)
                 current_time = time.time()
                 if (
                     event.src_path in last_executed
                     and (current_time - last_executed[event.src_path]) < EXECUTION_DELAY
                 ):
                     return
-                # 최신 크기 저장 & 실행 시간 갱신
                 last_executed[event.src_path] = current_time
                 print(f"이미지 파일 변경 감지: {event.src_path}")
 
-                # 변경된 이미지 파일을 처리하는 다른 파이썬 코드 실행
                 is_running = True
 
-        # subprocess.run을 비동기적으로 실행
         threading.Thread(target=self.run_subprocess, args=(event.src_path,)).start()
 
     def run_subprocess(self, src_path):
@@ -44,7 +41,7 @@ class ImageFileEventHandler(FileSystemEventHandler):
 
 
 if __name__ == "__main__":
-    path_to_watch = r"C:/Users/UserK/Desktop/raw"  # 감시할 폴더 경로
+    path_to_watch = r"C:/Users/UserK/Desktop/raw"
 
     observer = Observer()
     event_handler = ImageFileEventHandler()
