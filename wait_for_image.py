47ff9c621817b4697b99dcc2a0b554ef0e319301
WonYeongjun <1014wonyoung@snu.ac.kr>
2025-02-12 13:21:42 +0900

    일단 실행 과정에서 사용하는 파일이 raw.jpg인 버전으로는 됨

--- wait_for_image.py
+++ wait_for_image.py
@@ -2,38 +2,47 @@ import time
 import subprocess
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import threading
 
-file_sizes = {}  # 최근 변경된 파일 크기 저장
+# 전역 변수 설정
 last_executed = {}  # 파일별 마지막 실행 시간 저장
 EXECUTION_DELAY = 2  # 중복 실행 방지 대기 시간 (초)
 is_running = False  # 파이썬 코드 실행 중 여부
+lock = threading.Lock()  # 스레드 안전성을 위한 락
 
 
 class ImageFileEventHandler(FileSystemEventHandler):
     def on_modified(self, event):
         global is_running
-        if is_running:
-            return
-
-        if not event.is_directory and event.src_path.lower().endswith(
-            (".jpg", ".jpeg", ".png", ".gif")
-        ):
-            time.sleep(0.5)  # 파일 저장이 완료되도록 대기
-
-            current_time = time.time()
-            if (
-                event.src_path in last_executed
-                and (current_time - last_executed[event.src_path]) < EXECUTION_DELAY
-            ):
+        with lock:
+            if is_running:
                 return
-            time.sleep(3)
-            # 최신 크기 저장 & 실행 시간 갱신
-            last_executed[event.src_path] = current_time
-            print(f"이미지 파일 변경 감지: {event.src_path}")
 
-            # 변경된 이미지 파일을 처리하는 다른 파이썬 코드 실행
-            is_running = True
-            subprocess.run(["python", "calibration_marker_detector.py", event.src_path])
+            if not event.is_directory and event.src_path.lower().endswith(
+                (".jpg", ".jpeg", ".png", ".gif")
+            ):
+                time.sleep(0.5)  # 파일 저장이 완료되도록 대기
+
+                current_time = time.time()
+                if (
+                    event.src_path in last_executed
+                    and (current_time - last_executed[event.src_path]) < EXECUTION_DELAY
+                ):
+                    return
+                # 최신 크기 저장 & 실행 시간 갱신
+                last_executed[event.src_path] = current_time
+                print(f"이미지 파일 변경 감지: {event.src_path}")
+
+                # 변경된 이미지 파일을 처리하는 다른 파이썬 코드 실행
+                is_running = True
+
+        # subprocess.run을 비동기적으로 실행
+        threading.Thread(target=self.run_subprocess, args=(event.src_path,)).start()
+
+    def run_subprocess(self, src_path):
+        global is_running
+        subprocess.run(["python", "calibration_marker_detector.py", src_path])
+        with lock:
             is_running = False
 
 
