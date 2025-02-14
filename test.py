c794b653a08d19a77d6ec69f2882d475d464abe9
WonYeongjun <1014wonyoung@snu.ac.kr>
2025-02-14 23:18:56 +0900

    vit_h로 했는데 저건 용량이 너무 커서 바탕화면에 빼놓음

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
+import cv2
+import numpy as np
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
 import torch
-
-print("PyTorch 버전:", torch.__version__)
-print("CUDA 사용 가능 여부:", torch.cuda.is_available())
-
-if torch.cuda.is_available():
-    print("사용 가능한 GPU 개수:", torch.cuda.device_count())
-    print("현재 사용 중인 GPU:", torch.cuda.get_device_name(0))
-else:
-    print("GPU를 사용할 수 없습니다.")
