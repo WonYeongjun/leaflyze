014d1b83639ea4c36c2457599a85202b32b0c214
WonYeongjun <1014wonyoung@snu.ac.kr>
2025-02-17 10:16:03 +0900

    test에 cuda 되는지 보는 코드 넣어놓음

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ import cv2
 import numpy as np
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
 import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
