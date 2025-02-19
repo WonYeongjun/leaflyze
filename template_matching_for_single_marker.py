f71835a475c2ff77c8fcce93246bb441ebe50a57
WonYeongjun <1014wonyoung@snu.ac.kr>
2025-02-19 11:12:38 +0900

    전체 이미지 한 곳에 저장하도록 함

--- template_matching_for_single_marker.py
+++ template_matching_for_single_marker.py
@@ -5,6 +5,7 @@ import time
 import cv2
 import matplotlib
 from matplotlib import pyplot, patches
+import math
 
 from invariant_match_template import invariant_match_template
 from get_point_of_interest import get_point_of_interest
@@ -13,7 +14,7 @@ from simplication import morphlogy_diff
 start_time = time.time()
 
 
-example_fabric_type = "white"
+example_fabric_type = "pink"
 
 
 class PointInfo:
@@ -28,7 +29,12 @@ class PointInfo:
 if __name__ == "__main__":
     ans_list = []
     image_files = glob.glob(f"./image/{example_fabric_type}/*.jpg")
-    for file in image_files:
+    grid_cols = 2
+    grid_rows = math.ceil(len(image_files) / grid_cols)
+    fig, axes = pyplot.subplots(grid_rows, grid_cols, figsize=(15, 15))
+
+    pyplot.gcf().canvas.manager.set_window_title("Template Matching Results: Grid")
+    for index, file in enumerate(image_files):
         img_bgr = cv2.imread(file)
         img_bgr = get_point_of_interest(img_bgr)
         _, _, img_gray = morphlogy_diff(img_bgr)
@@ -37,7 +43,7 @@ if __name__ == "__main__":
         template_bgr = cv2.imread("./image/marker_4.png")
         template_bgr = cv2.resize(
             template_bgr, (0, 0), fx=1, fy=1
-        )  # TODO: 템플릿 사이즈 조절(초기 설정 필요)
+        )  # TODO: 템플릿 사이즈 조절
         template_gray = cv2.cvtColor(template_bgr, cv2.COLOR_RGB2GRAY)
         template_blur = cv2.GaussianBlur(template_gray, (11, 11), 0)
         height, width = template_blur.shape
@@ -63,15 +69,11 @@ if __name__ == "__main__":
             for point_info in result
         ]
 
-        figure, image_axes = pyplot.subplots(1)
-        pyplot.gcf().canvas.manager.set_window_title(
-            "Template Matching Results: Rectangles"
-        )
-
-        image_axes.imshow(img_rgb)
+        local_axes = axes[index // grid_cols, index % grid_cols]
+        local_axes.imshow(img_rgb)
         point_info_list = point_info_list[:10]
         for i, point_info in enumerate(point_info_list):
-            pyplot.scatter(
+            local_axes.scatter(
                 point_info.x + (width / 2) * point_info.scale[0] / 100,
                 point_info.y + (height / 2) * point_info.scale[1] / 100,
                 s=20,
@@ -81,7 +83,7 @@ if __name__ == "__main__":
                 point_info.x + (width / 2) * point_info.scale[0] / 100,
                 point_info.y + (height / 2) * point_info.scale[1] / 100,
             )
-            pyplot.scatter(point_info.x, point_info.y, s=20, color="green")
+            local_axes.scatter(point_info.x, point_info.y, s=20, color="green")
             rectangle = patches.Rectangle(
                 (point_info.x, point_info.y),
                 width * point_info.scale[0] / 100,
@@ -90,7 +92,7 @@ if __name__ == "__main__":
                 alpha=0.50,
                 label="Matched box",
             )
-            pyplot.text(
+            local_axes.text(
                 point_info.x,
                 point_info.y - 10,
                 f"{str(i)} : {point_info.score:.3f}",
@@ -104,18 +106,20 @@ if __name__ == "__main__":
                     point_info.y + height / 2 * point_info.scale[1] / 100,
                     point_info.angle,
                 )
-                + image_axes.transData
+                + local_axes.transData
             )
             rectangle.set_transform(transform)
-            image_axes.add_patch(rectangle)
-
-        pyplot.legend(handles=[rectangle])
-        pyplot.grid(True)
-        file_name = os.path.basename(file)
-        image_save_path = f"./output/{example_fabric_type}/output_{file_name}"
-        pyplot.savefig(image_save_path, dpi=300)
+            local_axes.add_patch(rectangle)
+            local_axes.legend(handles=[rectangle])
+            local_axes.grid(True)
 
         ans_list.append([point_info.score for point_info in point_info_list[:5]])
+
+    pyplot.tight_layout()
+    file_name = os.path.basename(file)
+    image_save_path = f"./output/{example_fabric_type}/output_{example_fabric_type}.png"
+    pyplot.savefig(image_save_path, dpi=300)
+    pyplot.show()
 
     file_path = f"./output/{example_fabric_type}/output_{example_fabric_type}.txt"
     with open(file_path, "w") as file:
