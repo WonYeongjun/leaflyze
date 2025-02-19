d39b2f8b6ff93339ece25b3d5ac593ce992ccf88
WonYeongjun <1014wonyoung@snu.ac.kr>
2025-02-19 18:02:15 +0900

    함수로 써먹을 수 있게 바꿨음

--- shape_detect.py
+++ shape_detect.py
@@ -5,34 +5,28 @@ from simplification import morphology_diff
 import matplotlib.pyplot as plt
 
 
-# 이미지 로드
-image_path = "C:/Users/UserK/Desktop/fin/fin_img.jpg"
-image = cv2.imread(image_path)
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-img_gray, _, _ = morphology_diff(image)
-# img_gray = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2GRAY)
-height, width = img_gray.shape
+def detect_shape(image):
+    height, width = img_gray.shape
 
-edges = cv2.Canny(img_gray, 50, 150)
-mask = np.ones_like(edges)
-lines = cv2.HoughLinesP(
-    edges, 1, np.pi / 180, threshold=100, minLineLength=30, maxLineGap=10
-)
-if lines is not None:
-    for line in lines:
-        x1, y1, x2, y2 = line[0]
-        cv2.line(mask, (x1, y1), (x2, y2), 0, 2)
+    edges = cv2.Canny(img_gray, 50, 150)
+    drawing_paper = np.ones_like(edges)
+    lines = cv2.HoughLinesP(
+        edges, 1, np.pi / 180, threshold=100, minLineLength=30, maxLineGap=10
+    )
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(drawing_paper, (x1, y1), (x2, y2), 0, 2)
+    return drawing_paper
 
-plt.figure(figsize=(10, 5))
 
-plt.subplot(1, 2, 1)
-plt.imshow(edges, cmap="gray")
-plt.title("Morphological Transformation")
-plt.axis("off")
-
-plt.subplot(1, 2, 2)
-plt.imshow(mask, cmap="gray")
-plt.title("Detected Lines")
-plt.axis("off")
-
-plt.show()
+if __name__ == "__main__":
+    image_path = "C:/Users/UserK/Desktop/fin/fin_img.jpg"
+    image = cv2.imread(image_path)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    img_gray, _, _ = morphology_diff(image)
+    shape_image = detect_shape(img_gray)
+    plt.imshow(shape_image, cmap="gray")
+    plt.title("Detected Lines")
+    plt.axis("off")
+    plt.show()
