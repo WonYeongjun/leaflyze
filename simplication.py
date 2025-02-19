13db4cc50f1719d9f49f4e9f9b7d8c29bf572c22
WonYeongjun <1014wonyoung@snu.ac.kr>
2025-02-19 11:41:41 +0900

    변수 이름 설명 자세하게 수정함

--- simplication.py
+++ simplication.py
@@ -6,24 +6,19 @@ def morphlogy_diff(image):
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (31, 31))
     img_morphed = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
 
-    difference = cv2.absdiff(image, img_morphed)
-    difference_gray = cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)
-    difference_gray = 255 - difference_gray
+    difference_with_morph = cv2.absdiff(image, img_morphed)
+    difference_with_morph_gray = cv2.cvtColor(difference_with_morph, cv2.COLOR_BGR2GRAY)
+    difference_with_morph_gray_inversed = 255 - difference_with_morph_gray
 
-    blurred = cv2.GaussianBlur(difference_gray, (9, 9), 10)
-    sharp = cv2.addWeighted(difference_gray, 1.5, blurred, -0.5, 0)
+    blurred = cv2.GaussianBlur(difference_with_morph_gray_inversed, (9, 9), 10)
+    contour_emphasized = cv2.addWeighted(
+        difference_with_morph_gray_inversed, 1.5, blurred, -0.5, 0
+    )
 
-    img_binary = cv2.threshold(sharp, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    return difference_gray, sharp, img_binary
-
-
-def add(a, b):
-    return a + b
-
-
-def side_effect_add(a, b):
-    a = a + b
-    return a
+    img_binary = cv2.threshold(
+        contour_emphasized, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+    )[1]
+    return difference_with_morph_gray_inversed, contour_emphasized, img_binary
 
 
 if __name__ == "__main__":
@@ -31,7 +26,7 @@ if __name__ == "__main__":
     img = cv2.imread("./image/pink/fin_cal_img_20250207_141129.jpg")
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    difference_gray, sharp = morphlogy_diff(img)
+    difference_gray, sharp, img_binary = morphlogy_diff(img)
     plt.figure(figsize=(15, 5))
 
     plt.subplot(1, 3, 1)
@@ -40,13 +35,13 @@ if __name__ == "__main__":
     plt.axis("off")
 
     plt.subplot(1, 3, 2)
-    plt.title("Morphed Image")
+    plt.title("Sharped")
     plt.imshow(cv2.cvtColor(sharp, cv2.COLOR_BGR2RGB))
     plt.axis("off")
 
     plt.subplot(1, 3, 3)
-    plt.title("Difference Image (Gray)")
-    plt.imshow(difference_gray, cmap="gray")
+    plt.title("binary")
+    plt.imshow(img_binary, cmap="gray")
     plt.axis("off")
 
     plt.show()
