a446cf724d1cc92b6d690f3d5d162b2f6c8fbe89
WonYeongjun <1014wonyoung@snu.ac.kr>
2025-02-21 18:29:40 +0900

    2.5도씩 전체에서 찾고, 그 다음에 0.1도씩 한 번 더 찾기

--- template_matching_for_square.py
+++ template_matching_for_square.py
@@ -2,13 +2,10 @@ import time
 
 import numpy as np
 import cv2
-from matplotlib import pyplot
 
 
 from simplification import morphology_diff
-from shape_detect import detect_lines
-
-start_time = time.time()
+from shape_detect import line_detector
 
 
 def make_rect(size, angle):
@@ -16,11 +13,9 @@ def make_rect(size, angle):
     center = (size[0] * 1.2 // 2, size[1] * 1.2 // 2)
     rotated_rect = ((center), size, angle)
 
-    # Get the box points (corners of the rotated rectangle)
     box = cv2.boxPoints(rotated_rect)
     box = np.int32(box)
 
-    # Precompute the mask shape for this configuration
     cv2.polylines(mask, [box], isClosed=True, color=255, thickness=31)
 
     return mask
@@ -35,37 +30,71 @@ class PointInfo:
 
 
 if __name__ == "__main__":
-    file_name = "white_back"
+
+    file_name = "purple_back"
     image_path = f"C:/Users/UserK/Desktop/fin/{file_name}.jpg"
 
     img_bgr = cv2.imread(image_path)
-    # img_bgr = get_point_of_interest(img_bgr)
-    img_gray, _, _ = morphology_diff(img_bgr)
+
+    _, img_gray = morphology_diff(img_bgr)
     img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
-    shape_image = detect_lines(img_gray)
-    pyplot.imshow(shape_image, cmap="gray")
-    pyplot.show()
-    result = []
+    shape_image = line_detector(img_gray)
+
+    result_rough = []
     width = 1800  # 1686 #TODO: Change this to the actual size of the square
     height = 1300  # 1378 #TODO: Change this to the actual size of the square
-    for angle in range(-10, 11):
-        template_mask = make_rect((width, height), angle)
-        template = np.ones_like(template_mask) * 255
+    template = np.ones((int(height * 1.2), int(width * 1.2)), dtype=np.uint8) * 255
+    start_time = time.time()
+
+    for angle in range(-300, 301, 25):
+        template_mask = make_rect((width, height), angle / 10)
         hotmap = cv2.matchTemplate(
             shape_image, template, cv2.TM_CCORR, mask=template_mask
         )
-        # pyplot.imshow(hotmap, cmap="hot")
-        # pyplot.show()
-        max_value = np.max(hotmap)  # 최댓값
-        max_index = np.unravel_index(np.argmax(hotmap), hotmap.shape)  # 좌표 변환
-        print(f"Max value: {max_value}, Max index: {max_index}, angle: {angle}")
-        result.append(
+
+        max_value = np.max(hotmap)
+        max_index = np.unravel_index(np.argmax(hotmap), hotmap.shape)
+        print(f"Max value: {max_value}, Max index: {max_index}, angle: {angle/10}")
+        result_rough.append(
             [
                 max_index,
                 angle,
                 max_value,
             ]
         )
+    point_info_list_rough = [
+        PointInfo(
+            x=point_info[0][1],
+            y=point_info[0][0],
+            angle=point_info[1],
+            score=point_info[2],
+        )
+        for point_info in result_rough
+    ]
+
+    point_info_list_rough.sort(key=lambda point: point.score, reverse=True)
+    point_info_rough = point_info_list_rough[:2]
+    result = []
+    point_info_rough.sort(key=lambda point: point.angle)
+    print(point_info_rough[0].angle, point_info_rough[1].angle)
+
+    for angle in range(point_info_rough[0].angle, point_info_rough[1].angle, 1):
+        template_mask = make_rect((width, height), angle / 10)
+        hotmap = cv2.matchTemplate(
+            shape_image, template, cv2.TM_CCORR, mask=template_mask
+        )
+        max_value = np.max(hotmap)
+        max_index = np.unravel_index(np.argmax(hotmap), hotmap.shape)
+        print(f"Max value: {max_value}, Max index: {max_index}, angle: {angle/10}")
+        result.append(
+            [
+                max_index,
+                angle / 10,
+                max_value,
+            ]
+        )
+
+    end_time = time.time()
 
     point_info_list = [
         PointInfo(
@@ -77,25 +106,22 @@ if __name__ == "__main__":
         for point_info in result
     ]
     point_info_list.sort(key=lambda point: point.score, reverse=True)
-    point_info_list = point_info_list[:1]
+    point_info = point_info_list[0]
 
-    for point_info in point_info_list:
-        center = (point_info.x + 0.6 * width, point_info.y + 0.6 * height)
-        rotated_rect = ((center), (width, height), point_info.angle)
-        # Get the box points (corners of the rotated rectangle)
-        box = cv2.boxPoints(rotated_rect)
-        box = np.int32(box)
+    center = (point_info.x + 0.6 * width, point_info.y + 0.6 * height)
+    rotated_rect = ((center), (width, height), point_info.angle)
 
-        # Precompute the mask shape for this configuration
-        cv2.polylines(shape_image, [box], isClosed=True, color=255, thickness=31)
-    # Combine the original image and the shape image side by side
+    box = cv2.boxPoints(rotated_rect)
+    box = np.int32(box)
+
+    cv2.polylines(shape_image, [box], isClosed=True, color=255, thickness=31)
+
     combined_image = np.hstack((img_rgb, cv2.cvtColor(shape_image, cv2.COLOR_GRAY2RGB)))
 
-    # Save the combined image
     cv2.imwrite(
         f"./output/back/{file_name}_result.png",
         cv2.cvtColor(combined_image, cv2.COLOR_RGB2BGR),
     )
-    end_time = time.time()
+
     elapsed_time = end_time - start_time
     print(f"작업에 걸린 시간: {elapsed_time} 초")
