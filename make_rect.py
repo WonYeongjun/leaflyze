b9f0f6078eaf08aa5a7e5446084e5a8dec09fb64
WonYeongjun <1014wonyoung@snu.ac.kr>
2025-02-27 18:31:25 +0900

    단순히 테스트함

--- make_rect.py
+++ make_rect.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 
 def make_rect(size, angle, thickness=31):
@@ -19,11 +20,18 @@ def make_rect(size, angle, thickness=31):
 
 if __name__ == "__main__":
     # 사각형의 너비와 높이를 사용자로부터 입력받음
-    width = 1800  # 예시 너비
+    width = 2000  # 예시 너비
     height = 1300  # 예시 높이
     angle = 0
 
-    output_image = make_rect((width, height), angle)
+    output_image = make_rect((width, height), angle, 101)
+    output_mask = make_rect((width, height), angle, 41)
+    # Apply the mask to the output image
+    masked_image = cv2.bitwise_and(output_image, output_image, mask=output_mask)
+
+    cv2.imwrite("masked_rectangle.png", masked_image)
+    plt.imshow(output_image, cmap="gray")
+    plt.show()
     cv2.imwrite("rectangle_with_border.png", output_image)
     cv2.imshow("Generated Rectangle with Border", output_image)
     cv2.waitKey(0)
